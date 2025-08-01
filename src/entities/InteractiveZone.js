a180f17c936cf785fa1f84942e785cec7f2445a6
jpoiri <justin.poirier@gmail.com>
2025-08-01 16:55:41 -0300

    fix: fix issue renamed variables

--- src/entities/InteractiveZone.js
+++ src/entities/InteractiveZone.js
@@ -15,7 +15,7 @@ export default class InteractiveZone extends Phaser.GameObjects.Rectangle {
 		scene.add.existing(this);
 
 		this.on('pointerdown', () => {
-			const tiles = scene.tilemap.getTilesWithinWorldXY(
+			const tiles = scene.tileMap.getTilesWithinWorldXY(
 				this.x,
 				this.y,
 				this.width,
@@ -30,38 +30,38 @@ export default class InteractiveZone extends Phaser.GameObjects.Rectangle {
 				case TileAction.REPLACE:
 					for (let i = 0, len = tiles.length; i < len; i++) {
 						if (this.newTiles[i]) {
-							scene.tilemap.putTileAt(this.newTiles[i], tiles[i].x, tiles[i].y);
+							scene.tileMap.putTileAt(this.newTiles[i], tiles[i].x, tiles[i].y);
 						}
 					}
 					break;
 				case TileAction.DESTROY:
 					for (let i = 0, len = tiles.length; i < len; i++) {
-						scene.tilemap.removeTileAt(tiles[i].x, tiles[i].y);
+						scene.tileMap.removeTileAt(tiles[i].x, tiles[i].y);
 					}
 					break;
 				case TileAction.MOVE:
 					for (let i = 0, len = tiles.length; i < len; i++) {
-						scene.tilemap.removeTileAt(tiles[i].x, tiles[i].y);
+						scene.tileMap.removeTileAt(tiles[i].x, tiles[i].y);
 					}
 					switch (this.direction) {
 						case Direction.LEFT:
 							for (let i = 0, len = tiles.length; i < len; i++) {
-								scene.tilemap.putTileAt(tiles[i].index, tiles[i].x + this.velocity, tiles[i].y);
+								scene.tileMap.putTileAt(tiles[i].index, tiles[i].x + this.velocity, tiles[i].y);
 							}
 							break;
 						case Direction.RIGHT:
 							for (let i = 0, len = tiles.length; i < len; i++) {
-								scene.tilemap.putTileAt(tiles[i].index, tiles[i].x - this.velocity, tiles[i].y);
+								scene.tileMap.putTileAt(tiles[i].index, tiles[i].x - this.velocity, tiles[i].y);
 							}
 							break;
 						case Direction.UP:
 							for (let i = 0, len = tiles.length; i < len; i++) {
-								scene.tilemap.putTileAt(tiles[i].index, tiles[i].x, tiles[i].y - this.velocity);
+								scene.tileMap.putTileAt(tiles[i].index, tiles[i].x, tiles[i].y - this.velocity);
 							}
 							break;
 						case Direction.DOWN:
 							for (let i = 0, len = tiles.length; i < len; i++) {
-								scene.tilemap.putTileAt(tiles[i].index, tiles[i].x, tiles[i].y + this.velocity);
+								scene.tileMap.putTileAt(tiles[i].index, tiles[i].x, tiles[i].y + this.velocity);
 							}
 							break;
 					}
